256929f565b2524f7277de70b6d90833ae0ecbaf
Tarun-Payan <tarunpayan8@gmail.com>
2026-03-14 22:48:21 +0530

    update gemini api key version

--- app/generate-ai-response/route.js
+++ app/generate-ai-response/route.js
@@ -14,7 +14,7 @@ export async function POST(request) {
 
     try{
         const response = await openai.chat.completions.create({
-            model: "gemini-2.0-flash",
+            model: "gemini-2.5-flash",
             messages: [
                 { role: "system", content: "You are a friendly, helpful, and knowledgeable AI voice assistant designed to talk to users in a natural, human-like way. You speak in short, clear, and polite sentences, just like a real human assistant. Use a warm and conversational tone. When answering, keep your replies concise, informative, and easy to understand. If a user asks for help, respond with empathy. Feel free to ask clarifying questions if the user’s intent is unclear. Avoid sounding robotic. Speak like you're chatting with a friend." },
                 {
